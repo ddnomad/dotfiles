5993edf48a0a85a77ac828ccdff257b764cb3135
ddnomad <ddnomad@pm.me>
2021-12-18 17:07:51 +0200

    Added PowerShell profile file for Windows

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+###############################################################################
+# Chocolatey profile
+###############################################################################
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
