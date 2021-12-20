975aa5be363790c6835e116c5c02f73e2af0195e
ddnomad <ddnomad@pm.me>
2021-12-20 02:42:21 +0200

    More VS code configuration

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,8 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
+
+###############################################################################
+# Enable Vi mode
+###############################################################################
+Set-PSReadlineOption -EditMode vi
