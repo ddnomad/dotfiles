804a075dc92640a5fc5841efa2b0a21f496136c2
ddnomad <self@ddnomad.net>
2023-02-11 01:48:15 +0000

    Fix clipboard configuration in VS Code Neovim config

--- .chezmoitemplates/neovim_settings.lua
+++ .chezmoitemplates/neovim_settings.lua
@@ -9,4 +9,11 @@ vim.g.mapleader = ';'
 
 vim.keymap.set('n', '<leader>r', RELOAD_CONFIG_COMMAND, {silent = true})
 
+{{ if eq .chezmoi.os "darwin" }}
+vim.opt.clipboard = "unnamed"
+{{ else }}
+vim.opt.clipboard = "unnamedplus"
+{{ end }}
+
 vim.opt.guicursor = 'n-v-c-sm:hor20,i-ci-ve:hor20,r-cr-o:hor20'
+
