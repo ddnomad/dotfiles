eb7711cfddf8a3afbdf885a905e5a8cd1b8df847
ddnomad <self@ddnomad.net>
2023-06-22 16:41:55 +0100

    Add support for showing hostname in Bash prompt if SSH'ed into the host

--- dot_bash/prompt.bash
+++ dot_bash/prompt.bash
@@ -1,26 +1,44 @@
 function assemble_prompt {
+    # Show the hostname if inside an SSH session
+    local host_token
+    host_token=''
+
+    if test -n "${SSH_CLIENT}"; then
+        local hostname
+
+        # Surprisingly, not every host has hostname command installed by default
+        if command -v hostname &> /dev/null; then
+            hostname="$(hostname)"
+        else
+            hostname="$(cat /proc/sys/kernel/hostname)"
+        fi
+
+        host_token=" ${hostname} "
+    fi
+
+    # Show current git branch if the current directory is under VCS
     local git_token
     git_token=''
 
     local git_branch
-    git_branch=$(git branch 2>/dev/null | sed -n "s/* \(.*\)/\1/p")
+    git_branch=$(git branch 2>/dev/null | sed -n 's/* \(.*\)/\1/p')
 
     if [ -n "${git_branch}" ]; then
         git_token=" ${git_branch} "
     fi
 
-    # Support for custom Python VirtualEnv prompt. This also relies on export VIRTUAL_ENV_DISABLE_PROMPT=1
+    # Show Python VirtualEnv envirnment name. This also relies on export VIRTUAL_ENV_DISABLE_PROMPT=1
     # being set in .bashrc, otherwise VirtualEnv will append its own ugly af prompt prefix as well.
     local virtualenv_token
     virtualenv_token=''
 
     if [ -n "${VIRTUAL_ENV}" ]; then
 
-        # Running inside a local project environment created by Poetry / PDM etc.
-        if [[ "${VIRTUAL_ENV}" == *".venv"* ]]; then
+        # Running inside a local project environment created by Poetry
+        if [[ "${VIRTUAL_ENV}" == *'.venv'* ]]; then
                 virtualenv_name="$(echo "${VIRTUAL_ENV}" | rev | cut -c7- | rev | xargs basename | tr -d $'\n' | tr -d $'\r')"
 
-        # Running inside an environment created by Pipenv / Poetry / other tool outside of the project directory
+        # Running inside an environment created by Pipenv or Poetry outside of the project directory
         else
                 loca virtualenv_name
                 virtualenv_name="$(basename "${VIRTUAL_ENV}" | tr -d $'\n' | tr -d $'\r')"
@@ -30,13 +48,13 @@ function assemble_prompt {
         virtualenv_token=" ${virtualenv_name} "
     fi
 
-    echo -n "${git_token}${virtualenv_token}"
+    echo -n "${host_token}${git_token}${virtualenv_token}"
 }
 
-# shellcheck disable=SC2016
-printf -v PS1 "%s" \
-    '$(if [[ $? != 0 ]]; then echo " \[\e[91m\]\[\e[0m\]";' \
-    'fi) \u \[\e[90m\]$(assemble_prompt)\[\e[0m\]$ '
 
-# Set line continuation prompt
+# shellcheck disable=SC2016
+printf -v PS1 "%s" '$(if [[ $? != 0 ]]; then echo " \[\e[91m\]\[\e[0m\]"; fi) \u \[\e[90m\]$(assemble_prompt)\[\e[0m\]$ '
+
+
 PS2=" ↳ "
+
